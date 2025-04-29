8fc9bd8eec543d0562cd878d91a5c9d274d81683
matteo <matteoavella8@gmail.com>
2025-04-29 20:54:53 +0200

    Aggiunta codice per creazione grafico complicazioni

--- main.py
+++ main.py
@@ -3,49 +3,85 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
 
+
+#FORMATTAZIONE DELLA TABELLA MOSTRATA DA TERMINALE
+############################################################################################
 # mostra tutte le colonne
 pd.set_option('display.max_columns', None)
 # allarga la “larghezza” massima della console (in caratteri)
 pd.set_option('display.width', 1000)
 # opzionale: non troncare i contenuti delle singole celle
 pd.set_option('display.max_colwidth', None)
-
-
-# pd.set_option('display.float_format', '{:.2f}'.format)
-
-# df = pd.read_csv("Excel/Biochemical_parameters.csv")
-df = pd.read_csv("Excel/Diagnostics.csv")
-# df = pd.read_csv("Excel/Glucose_measurements.csv")
-# df = pd.read_csv("Excel/Patient_info.csv")
-
-# Raggruppa per la colonna 'Description' e conta il numero di pazienti in ogni gruppo
-grouped = df.groupby('Description').size()
-
-# Ordina per numero di pazienti in ordine decrescente e prendi le prime 20 descrizioni
-top_20_descriptions = grouped.sort_values(ascending=False).head(10)
-
-# Filtra il dataframe originale per includere solo i record con le descrizioni delle prime 20
-filtered_df = df[df['Description'].isin(top_20_descriptions.index)]
-
-# Stampa le prime 20 descrizioni con il numero di pazienti
-print(top_20_descriptions)
-print()
-
+############################################################################################
 
 
 # LEGENDA STATISTICHE
+############################################################################################
 # mean = MEDIA
 # std = DEVIAZIONE STANDARD
 # min = VALORE MINIMO
-# 25% = PRIMO QUARTILE il punto sotto cui si trova il 25% dei dati
+# 25% = PRIMO QUARTILE: il punto sotto cui si trova il 25% dei dati
 # 50% = MEDIANA / SECONDO QUARTILE
 # 75% = TERZO QUARTILE
 # max = VALORE MASSIMO
+############################################################################################
 
-# for index, row in df.iterrows():
-#     print(row)
+#SETTARE I RISULTATI DA TERMINALE CON SOLO 2 CIFRE DECIMALI
+# pd.set_option('display.float_format', '{:.2f}'.format)
+
+
+#DEFINIZIONE DATASET
+############################################################################################
+# df = pd.read_csv("Excel/Biochemical_parameters.csv")
+# df = pd.read_csv("Excel/Diagnostics.csv")
+# df = pd.read_csv("Excel/Glucose_measurements.csv")
+# df = pd.read_csv("Excel/Patient_info.csv")
+############################################################################################
+
+
+#CREAZIONE GRAFICO SULLE PRIME 22 DIAGNOSI CON PIU PAZIENTI AFFETTI
+#DECOMMENTARE Excel/Diagnostics.csv PER ESEGUIRE QUESTO PEZZO DI CODICE
+############################################################################################
+# # Raggruppa per la colonna 'Description' e conta il numero di pazienti in ogni gruppo
+# grouped = df.groupby('Description').size()
+#
+# # Ordina i dati in ordine decrescente
+# top_22_descriptions = grouped.sort_values(ascending=False).head(22)
+#
+# fig, ax = plt.subplots(figsize=(12,8))
+#
+# # disegna il barplot
+# sns.barplot(
+#     x=top_22_descriptions.index,
+#     y=top_22_descriptions.values,
+#     hue=top_22_descriptions.index,
+#     palette="viridis",
+#     legend=False,
+#     ax=ax
+# )
+#
+# # prendi le etichette originali, applica wrapping ogni 15 caratteri
+# wrapped = top_22_descriptions.index.to_series().str.wrap(55)
+#
+# # impostale sull'asse x, con rotazione
+# ax.set_xticklabels(wrapped, rotation=45, ha='right')
+#
+# ax.set_title('Numero di pazienti per diagnosi', fontsize=20)
+# ax.set_ylabel('Numero di pazienti', fontsize=16)
+# ax.set_xlabel('')  # niente label orizzontale
+#
+# plt.tight_layout()
+# plt.show()
 #
 #
+# # Stampa le prime 22 descrizioni con il numero di pazienti
+# print(top_22_descriptions)
+# print()
+############################################################################################
+
+
+#STAMPA DELLE STATISTICHE DEL DATASET
+############################################################################################
 # print()
 # print('numero di righe e colonne')
 # print(df.shape)
@@ -55,26 +91,31 @@ print()
 # print()
 # print('statistiche numeriche di base')
 # print(df.describe()) #viene eseguito solo sulle colonne numeriche es:int64
+############################################################################################
 
 
-# diagnostics_df = pd.read_csv("Excel/Diagnostics.csv")
-# biochemical_df = pd.read_csv("Excel/Biochemical_parameters.csv")
-# # pivot dei parametri
-# bio_wide = biochemical_df.pivot_table(
-#     index="Patient_ID",
-#     columns="Name",
-#     values="Value",
-#     aggfunc="mean"
-# ).reset_index()
-#
-# # merge wide
-# merged_wide = pd.merge(
-#     diagnostics_df,
-#     bio_wide,
-#     on="Patient_ID",
-#     how="left"
-# )
+#MERGE DELLE TABELLE Diagnostics.csv E Biochemical_parameters.csv E STAMPA DELLE STATISTICHE E
+#DEI PAZIENTI CHE HANNO UNO SPECIFICA COMPLICAZIONE (Other and unspecified hyperlipidemia)
+############################################################################################
+diagnostics_df = pd.read_csv("Excel/Diagnostics.csv")
+biochemical_df = pd.read_csv("Excel/Biochemical_parameters.csv")
+# pivot dei parametri
+bio_wide = biochemical_df.pivot_table(
+    index="Patient_ID",
+    columns="Name",
+    values="Value",
+    aggfunc="mean"
+).reset_index()
 
+# merge wide
+merged_wide = pd.merge(
+    diagnostics_df,
+    bio_wide,
+    on="Patient_ID",
+    how="left"
+)
+
+#PRINT STATISTICHE
 # print()
 # print('numero di righe e colonne')
 # print(merged_wide.shape)
@@ -84,18 +125,18 @@ print()
 # print()
 # print('statistiche numeriche di base')
 # print(merged_wide.describe()) #viene eseguito solo sulle colonne numeriche es:int64
+# print()
 
 
 # filtri solo le righe con Description == 'Other and unspecified hyperlipidemia'
-# subset = merged_wide[ merged_wide['Description'] == 'Other and unspecified hyperlipidemia' ]
-#
-# nan_mask = subset.isna()
-# nan_count = nan_mask.sum()
-#
-# print(nan_count)
+subset = merged_wide[ merged_wide['Description'] == 'Other and unspecified hyperlipidemia' ]
 
-# poi le stampi tutte insieme
-# print(subset)
 
-# for index, row in merged_wide.iterrows():
-#     print(row)
+#PRINT DI QUANTI VALORI NULLI HA OGNI COLONNA DELLA TABELLA
+nan_mask = subset.isna()
+nan_count = nan_mask.sum()
+print(nan_count)
+
+#PRINT LISTA PAZIENTI CON QUESTA COMPLICAZIONE (Other and unspecified hyperlipidemia)
+print(subset)
+############################################################################################
